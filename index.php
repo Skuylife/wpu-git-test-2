c3f3341c8fc2c1bb6d401698e04141e81be972e0
Hy Arafah <arafahadam4@gmail.com>
2023-10-29 22:14:28 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello wolrd</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, dignissimos? Rerum odit rem nobis necessitatibus ex placeat sapiente, exercitationem nihil commodi quasi cupiditate perferendis voluptatum id, voluptate aliquam? Dignissimos, quisquam.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
